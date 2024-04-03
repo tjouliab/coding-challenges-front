eafe3b7181e4deeee84cfda62f20b1b458caa02f
tjouliab <tim.jouliab@icloud.com>
2024-04-03 14:26:24 +0200

    implement endpoint find file by name

--- src/components/1-wc-tool/WcTool.tsx
+++ src/components/1-wc-tool/WcTool.tsx
@@ -1,42 +1,86 @@
 import { useEffect, useState } from "react";
-import { WcToolService } from "../../services/WcToolService";
+import { FileDto, WcToolService } from "../../services/WcToolService";
 import "./WcTool.css";
 
 function WcTool() {
-  const [selectedFileName, setSelectedFileName] = useState<string>("");
   const [fileNames, setFileNames] = useState<string[]>([]);
+  const [selectedFileName, setSelectedFileName] = useState<string>("");
+  const [selectedFile, setSelectedFile] = useState<FileDto>();
 
   useEffect((): void => {
     WcToolService.getAllFilesNames()
       .then((res) => {
-        console.log("res", res);
         setFileNames(res);
       })
       .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+  });
+
+  function handleOptionClick(fileName: string): void {
+    setSelectedFileName(fileName);
+    WcToolService.getFileByName(fileName)
+      .then((res: FileDto) => {
+        setSelectedFile(res);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }
+
+  function getTextToDisplay(startContent: string, endContent: string): string {
+    return `${startContent} ${endContent ? `[...] ${endContent}` : ""}`;
+  }
 
   return (
     <>
-      <div className="dropdown">
-        <button
-          className="btn btn-secondary dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-          // onClick={onclick()}
-        >
-          Select a file
-        </button>
-        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          {fileNames.map((fileName) => (
-            <li key={fileName}>
-              <a className="dropdown-item" href="#">
-                {fileName}
-              </a>
-            </li>
-          ))}
-        </ul>
+      <div className="exercice-container">
+        <h1>Word Count Tool</h1>
+        <div className="actions-container">
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Select a file
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              {fileNames.map((fileName) => (
+                <li key={fileName}>
+                  <a
+                    className={`dropdown-item ${
+                      selectedFileName === fileName ? "active" : ""
+                    }`}
+                    href="#"
+                    onClick={() => handleOptionClick(fileName)}
+                  >
+                    {fileName}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <h4 className="text-separator">OR</h4>
+          <button className="btn btn-secondary" type="button">
+            Download
+          </button>
+        </div>
+
+        {selectedFile ? (
+          <div className="input-group">
+            <span className="input-group-text">{selectedFile.name}</span>
+            <textarea
+              readOnly
+              className="form-control"
+              aria-label="With textarea"
+              value={getTextToDisplay(
+                selectedFile.startContent,
+                selectedFile.endContent
+              )}
+            ></textarea>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
--- src/services/WcToolService.ts
+++ src/services/WcToolService.ts
@@ -1,11 +1,25 @@
 import { REACT_APP_BASE_URL } from "../../EnvironmentVariables";
 
+export interface FileDto {
+  name: string;
+  startContent: string;
+  endContent: string;
+}
+
 const baseUrl = `${REACT_APP_BASE_URL}/wc-tool`;
 
 export const WcToolService = {
   getAllFilesNames: async (): Promise<string[]> => {
     try {
       return (await fetch(`${baseUrl}/all-files-names`)).json();
+    } catch (error) {
+      throw new Error(error as string);
+    }
+  },
+
+  getFileByName: async (fileName: string): Promise<FileDto> => {
+    try {
+      return (await fetch(`${baseUrl}/file-by-name?name=${fileName}`)).json();
     } catch (error) {
       throw new Error(error as string);
     }
